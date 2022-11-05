de8936c2ea5d51c79d9bc9cb9adaffcf421ad218
agiltriono <agiltriono71@yahoo.com>
2022-11-06 00:16:45 +0700

    Update BlockedChannel

--- event/messageCreate.js
+++ event/messageCreate.js
@@ -8,6 +8,7 @@ module.exports = {
     if (message.author.bot) return;
     if (!message.guild) return;
     db.child(message.guild.id).once("value", async function(data) {
+      const blockedchannel = data.child("bc")
       const prefix = data.child("prefix").val() ? data.child("prefix").val() : client.prefix;
       const cc = data.child("cc")
       const args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -20,6 +21,7 @@ module.exports = {
       const viewsend = message.guild.me.permissions.has("VIEW_CHANNEL") && message.guild.me.permissions.has("SEND_MESSAGES");
       const manages = message.guild.me.permissions.has("MANAGE_MESSAGES") && message.guild.me.permissions.has("MANAGE_CHANNELS");
       const hasPerm = viewsend && manages;
+      if(blockedchannel.exists() && blockedchannel.val().includes(message.channel.id)) return;
       if(!command && command != helpString && cc.exists()) {
         let phrase = message.content.replace(/\n/g, ' ')
         let index = [...cc.val()]
--- includes/setting/blockchannel.js
+++ includes/setting/blockchannel.js
@@ -3,15 +3,12 @@ const { database, clear, embeds, remove, color } = require(".././../util/util");
 const db = database.ref("guild")
 module.exports.execute = async function(interaction, client, userId) {
   const message = interaction.message
-  try {
   const guild = interaction.guild
   const member = interaction.guild.members.cache.get(interaction.user.id)
-  const regex = /^<#[0-9]*>$/;
   const selected = [...interaction.values]
   const description = message.embeds[0].description.trim()
-  const current = description.includes("Tidak ada channel") ? [] : [...description.split(",")].map(c=> c.trim().replace(regex, ""))
-  const pre_merged = current.length != 0 ? [...new Set(...current,...selected)] : [...selected]
-  const merged = pre_merged.filter(id => id && !selected.includes(id))
+  const current = description.includes("Tidak ada channel") ? [] : description.split(",").map(c=> c.replace(/[\\<>@#&!]/g, ""))
+  const merged = current.length != 0 ? [...new Set([...current.filter(id=> !selected.includes(id)),...selected.filter(id=>!current.includes(id))])] : [...selected]
   const ch = await guild.channels.cache.filter(c=>c.type === "GUILD_TEXT")
   const option = ch.map(c => {
     return {
@@ -45,9 +42,6 @@ module.exports.execute = async function(interaction, client, userId) {
     }],
     components: [].concat(button, menu)
   })
-  } catch (error) {
-    return message.channel.send(error)
-  }
 }
 async function chunk(obj, i, userId) {
   let chunks = [];

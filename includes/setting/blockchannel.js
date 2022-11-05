2c7299ce93a75c23dc797afd252da02800be63ee
agiltriono <agiltriono71@yahoo.com>
2022-11-05 22:54:42 +0700

    Fix Undefined styntax error#3 at Blocked Channels

--- includes/setting/blockchannel.js
+++ includes/setting/blockchannel.js
@@ -6,7 +6,7 @@ module.exports.execute = async function(interaction, client, userId) {
   try {
   const guild = interaction.guild
   const member = interaction.guild.members.cache.get(interaction.user.id)
-  const regex = /^<#[0-9]*>$/gm;
+  const regex = /^<#[0-9]*>$/;
   const selected = interaction.values
   const description = message.embeds[0].description.trim()
   const current = description.includes("Tidak ada channel") ? [] : description.split(",").map(c=> c.trim().replace(regex, ""))
@@ -36,11 +36,12 @@ module.exports.execute = async function(interaction, client, userId) {
       .addOptions(option))
     ]
   const menu = option.length > 25 ? await chunk(option, 25, userId) : simple
-  await db.child(guild.id).update({bc:merged.toString()})
+  if (merged.length == 0) await db.child(guild.id).child("bc").remove();
+  if (merged.length > 0) await db.child(guild.id).update({bc:merged.toString()});
   await interaction.update({
     embeds: [{
       title: "BLOCKED CHANNEL",
-      description: merged.map(c=> `<#${c}>`).toString()
+      description: merged.length != 0 ? merged.map(c=> `<#${c}>`).toString() : "Tidak ada channel"
     }],
     components: [].concat(button, menu)
   })

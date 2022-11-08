4a8981a93964fda96c576831d41c82d4cc60b24f
agiltriono <agiltriono71@yahoo.com>
2022-11-08 15:30:09 +0700

    Update

--- includes/custom/create.js
+++ includes/custom/create.js
@@ -23,6 +23,7 @@ module.exports.execute = async function(interaction, client, userId) {
     type: 1,
     components: [
       new MessageButton().setCustomId('cc_button_text_'+userId+"_"+commandName).setEmoji("🖍").setLabel("Edit Text").setStyle('PRIMARY'),
+      new MessageButton().setCustomId('cc_button_trigger_'+userId+"_"+commandName).setEmoji("⁉️").setLabel("Edit Trigger").setStyle('PRIMARY'),
       new MessageButton().setCustomId('cc_button_embed_'+userId+"_"+commandName).setEmoji("🔁").setLabel("Rich Embed").setStyle('PRIMARY'),
       new MessageButton().setCustomId('cc_button_reset_'+userId+"_"+commandName).setEmoji("♻️").setLabel("Reset").setStyle('PRIMARY')
       ]

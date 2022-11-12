a03796de6808702d63ab4c85a9e79927adf87ca6
agiltriono <agiltriono@gmail.com>
2022-11-12 12:23:13 +0000

    Huge Changes

--- event/messageCreate.js
+++ event/messageCreate.js
@@ -10,10 +10,10 @@ module.exports = {
     if (!message.guild) return;
     db.child(message.guild.id).once("value", async function(data) {
       const blockedchannel = data.child("bc")
-      const bad_word = data.child("badword")
-      const wordlist = bad_word.child("word").val();
-      const wordblock = bad_word.child("enable").val();
-      const log = bad_word.child("log").val();
+      const wb = data.child("wb")
+      const blockword = wb.child("word").val();
+      const blockenable = wb.child("enable").val();
+      const blocklog = wb.child("log").val();
       const prefix = data.child("prefix").val() ? data.child("prefix").val() : client.prefix;
       const cc = data.child("cc")
       const args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -26,9 +26,6 @@ module.exports = {
       const viewsend = message.guild.me.permissions.has("VIEW_CHANNEL") && message.guild.me.permissions.has("SEND_MESSAGES");
       const manages = message.guild.me.permissions.has("MANAGE_MESSAGES") && message.guild.me.permissions.has("MANAGE_CHANNELS");
       const hasPerm = viewsend && manages;
-      if (wordblock === true && wordlist != null && hasPerm) {
-        const badword =  new BadWord(message, client, languages, wordlist, infraction, punishment, log).init()
-      }
       if(blockedchannel.exists() && blockedchannel.val().includes(message.channel.id)) return;
       if (!command && command != helpString) {
         var date = new Date()
@@ -53,7 +50,7 @@ module.exports = {
           }
           //petang
           if (zone >= 18 && zone <= 19) {
-            if (string.includes("pagi") || string.includes("siang") || string.includes("sore") || string.includes("malam") || string.includes("dini")) return mesaage.channel.send(shuffle.pick(["Petang akak!","Inikan petang ish","bodo amat ah akak capek", "Apaan sih petang juga", "Setiap menjelang malam queen pasti selalu semangat tau!"], {"picks":1}));
+            if (string.includes("pagi") || string.includes("siang") || string.includes("sore") || string.includes("malam") || string.includes("dini")) return message.channel.send(shuffle.pick(["Petang akak!","Inikan petang ish","bodo amat ah akak capek", "Apaan sih petang juga", "Setiap menjelang malam queen pasti selalu semangat tau!"], {"picks":1}));
             if (string.includes("petang")) return message.channel.send("Menjelang malam 💬");
           }
           //malam

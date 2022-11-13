dc0d994d41630db9e8bf7ab111c0eec2a11f5132
agiltriono <agiltriono@gmail.com>
2022-11-13 09:37:00 +0000

    Huge Changes

---
+++ commands/voice/set-allow-link.js
@@ -0,0 +1,53 @@
+const { database, clear, embeds } = require(".././../util/util");
+const db = database.ref("guild");
+module.exports.help = {
+    name: "vc-allow-link",
+    aliases: ["vc-link"],
+    usage:"name",
+    category: "Setting",
+    permissions: ["SEND_MESSAGES"],
+    description: "Buat daftar url yang di izinkan pada Voice Chat."
+}
+
+module.exports.run = async (msg, args, creator, prefix) => {
+  await msg.delete()
+  const permis = [
+    (msg.member.permissions.has("ADMINISTRATOR")),
+    (msg.member.permissions.has("MANAGE_GUILD")),
+    (creator.id === msg.guild.ownerId)
+  ].filter(u=>u.toString() != "false")
+  if(permis.length === 0) return;
+  if (!msg.guild.me.permissions.has("SEND_MESSAGES")) return msg.channel.send(embeds("❌ Aku butuh permissions `SEND_MESSAGES`")).then(m=> clear(m, 3000));
+  const rgx = /^[a-zA-Z]+$/
+  const guild = msg.guild
+  if (args[0] != undefined && args[0].toLowerCase() === "add") {
+    // add
+    if (args[1] === undefined) return await msg.channel.send(embeds(`❌ **Salah perintah**\nTry It : \`${prefix}vc-link help\``));
+    let link = args[1].toLowerCase().trim()
+    db.child(guild.id).once("value", async (s) =>{
+      const src = s.child("voice").child("allow_link")
+      const list = src.exists() ? src.val().trim().split(",") : []
+      if (list.includes(link)) return await msg.channel.send(embeds(`⚠️ **${link}** sudah ada di dalam daftar.\n\nDaftar Saat ini :\n${list.join(",")}`));
+      list.push(link)
+      await db.child(msg.guild.id).child("voice").update({allow_link:list.toString()})
+      await msg.channel.send(embeds(`✅ Daftar di update.\n🆕 ${link}`))
+    })
+  } else if(args[0] != undefined && args[0].toLowerCase() === "del") {
+    // delete
+    if (args[1] === undefined) return await msg.channel.send(embeds(`❌ **Salah perintah**\nTry It : \`${prefix}vc-link help\``));
+    let link = args[1].toLowerCase().trim()
+    db.child(guild.id).once("value", async (s) =>{
+      const src = s.child("voice").child("allow_link")
+      const list = src.exists() ? src.val().trim().split(",") : []
+      if (list.length === 0) return await msg.channel.send(embeds(`⚠️ Daftar kosong, Tambahkan link baru ke daftar terlebih dahulu.`));
+      if (!list.includes(link)) return await msg.channel.send(embeds(`⚠️ **${link}** Tidak ada pada daftar!`));
+      let newList = list.filter(l=> !l.includes(link))
+      await db.child(msg.guild.id).child("voice").update({allow_link:list.toString()})
+      await msg.channel.send(embeds(`✅ Daftar di update.\n❌ ${link}`))
+    })
+  } else if (args[0] != undefined && args[0].toLowerCase() === "help") {
+    await msg.channel.send(embeds(`🛠 **__Allow Link On VC__**\n\nTambah Link ke daftar :\n \`${prefix}vc-link add youtube.com\`\nHapus Link Dari Daftar :\n \`${prefix}vc-link del youtube.com\`\n\n*NOTE: Link yang tidak ada pada daftar akan segera di hapus.*`));
+  } else {
+    await msg.channel.send(embeds(`❌ **Salah perintah**\nTry It : \`${prefix}vc-link help\``))
+  }
+};
--- util/linkremover.js
+++ util/linkremover.js
@@ -2,16 +2,11 @@ module.exports = async function linkremover(msg, text, voice){
   const channelId = msg.channelId
   const vc = voice
   const temp = vc.child("temp").child(channelId)
-  //const chat_filter = vc.child("chat_filter")
+  const vclink = vc.child("allow_link")
   const content = msg.content.toLowerCase()
-  if (temp.exists()) {
-    const filter = [
-      "youtu.be",
-      "youtube.com",
-      "soundcloud.com",
-      "spotify.com"
-    ]
-    if(!filter.some(c => content.includes(c))) return await msg.delete();
+  if (temp.exists() && vclink.exists()) {
+    const filter = vclink.val().trim().split(",");
+    if(filter.length != 0 && !filter.some(c => content.includes(c))) return await msg.delete();
   } else if (text.exists()) {
     const arr = [...text.val()]
     if(arr.some(c => c.channel.includes(channelId) && c.link.split(",").some(a => content.includes(a)))) return await msg.delete()

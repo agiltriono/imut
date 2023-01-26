f8ed1faea77e1c85d206ef10dd84cf462a1af7d6
agiltriono <agiltriono71@yahoo.com>
2023-01-26 23:41:10 +0700

    Remove

--- event/guildCreate.js
+++
@@ -1,7 +0,0 @@
-const { clear } = require("../util/util");
-module.exports = {
-  name : "guildCreate",
-  async execute(guild, client) {
-    // todo Create Initial database
-  }
-}
--- includes/goodbye/help.js
+++
@@ -1,4 +0,0 @@
-const { ephemeral } = require(".././../util/util")
-module.exports.execute = async function(interaction, client, userId) {
-  await interaction.reply(ephemeral("**__Bantuan__**\n\n**{member}** : Mention Member.\n**Test** : Lihat pratinjau Welcomer.\n**Tutup** : Untuk menutup pengaturan."))
-}
